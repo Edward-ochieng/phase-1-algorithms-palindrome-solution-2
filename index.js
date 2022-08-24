88d629d12ab66747e9c993acff7b362336484e90
Edward <edward.ochieng@student.moringaschool.com>
2022-08-24 15:52:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+for(i = 0; i < word.length / 2; i++){
+// check each letter from the corresponding letter to the end
+const j = word.length - 1 - i
+const startingLetter = word[i]
+const lastLetter = word[j]
+// if any letter does not match return false
+if(startingLetter !== lastLetter) return false
 }
+return true
+}
+
+//    racecar
+//    0123456
+  //  i     j
+
+//    racecar
+//    0123456
+  //   i   j
+
+  //    racecar
+  //    0123456
+    //    i j
+  
+//    racecar
+  //    0123456
+    //     ij
+  
+
 
 /* 
   Add your pseudocode here
+  if the first and the last letter are the same
+  return true
+
+  and if the second letter and the second last letter are the same.
+  return true
+
+  if the first letter and the second letter are not the same.
+  return false
 */
 
 /*
@@ -20,6 +55,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+console.log("Expecting: true"),
+console.log("=>", isPalindrome('abba'))
+console.log(ex)
+
+
 }
 
 module.exports = isPalindrome;
